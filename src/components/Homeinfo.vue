e207610373c53e55995abba6457f0058bfdba634
Gilchrist32 <32stephencurry@gmail.com>
2020-07-28 20:18:19 +0800

    Added Query Script

--- src/components/Homeinfo.vue
+++ src/components/Homeinfo.vue
@@ -8,6 +8,7 @@
                     <h1 id="mname" class="h6-responsive font-weight-bold">GILCHRIST CALUNIA</h1>
                     <h6 id="quo" class="mb-4 font-weight-bold">Information Technology, Front-end Web Developer.</h6>
                       <hr class="my-4"> 
+                      <h3 class="ttitle" v-for=" i in cal_subscriber.slice(-1)" :key="i"> {{ i.id }} <span id="n">Subscribers</span></h3>
                   </div>
                    <v-col class="mt-xl-1 ml-3">
                       <v-img src="../assets/photo/home.png" width="320px" height="300px"></v-img>
@@ -19,9 +20,38 @@
 </template>
 
 <script>
+import { GET_ALL_SUBSCRIBER_QUERY } from '../graphql/queries'
+import { GET_ALL_SUBSCRIBER_QUERY_SUBSCRIPTION } from '../graphql/subscriptions'
 
 export default {
   name: 'Homeinfo',
+
+  data () {
+    return {
+        cal_subscriber: []
+    }
+  },
+
+   apollo: {
+    cal_subscriber: {
+      query: GET_ALL_SUBSCRIBER_QUERY,
+      subscribeToMore: {
+        document: GET_ALL_SUBSCRIBER_QUERY_SUBSCRIPTION,
+        updateQuery(previousResult, { subscriptionData }) {
+          if (previousResult) {
+            return {
+              subscribers: [
+                ...subscriptionData.data.cal_subscriber
+              ]
+            }
+          }
+        }
+      },
+      result ({ data }) {
+        this.counter = data.cal_subscriber
+      }
+    }
+  }
 }
 </script>
 
@@ -47,5 +77,14 @@ export default {
     font-weight: bolder;
     color: #ffffff;
     }
-
+#n{
+  font-size: 35px;
+  color: #fff;
+}
+.ttitle {
+  font-family: 'Didact Gothic';
+  color: #FFC400;
+  font-size: 35px;
+  font-weight: bolder;
+}
 </style>
