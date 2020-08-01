a65a424de3865286871939abd8f72f58af0b3601
Gilchrist32 <32stephencurry@gmail.com>
2020-08-01 15:39:23 +0800

    Updated the Carousel Components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
 <script>
 import Homeinfo from '../components/Homeinfo.vue';
 import Aboutme from '../components/Aboutme.vue';
-import Carousel from '../components/Carousel.vue'
+import Carousel from '../components/mixim/Carousel.vue'
 
 export default {
   name: 'Home',
