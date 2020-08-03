5ce56d87fa7316019482278f6030fa1d9ebbbc9d
Gilchrist32 <32stephencurry@gmail.com>
2020-08-03 23:13:56 +0800

    Added Query

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,73 +1,70 @@
 <template>
   <div class="project">
-    <v-container fluid fill-height pt-0>
-    <v-row>
-      <v-col>
+    <v-container fluid fill-height pt-0 >
+    <v-row > 
+      <v-col v-for="proj in project" :key="proj.id" >
         <v-card mt-4 max-width="500" class="mx-auto mt-4" id="card-project">
-          <v-list-item>
-            <v-list-item-avatar size="70">
-              <img src="../assets/photo/gilchrist.png" alt="gilchrist">
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title class="headline white--text" id="head">GC Official-Origin</v-list-item-title>
-              <v-list-item-subtitle id="name">by Gilchrist Calunia</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-img src="../assets/project/project2.png" height="194" ></v-img>
+<!--v-for="proj in project" :key="proj.id"-->
+          <v-img :src="proj.image_url" height="194" ></v-img>
 
           <v-card-text id="comment">
-            <h3>Portfolio</h3>
-            <hr class="lime accent-4 mb-2 mt-0 d-inline-block mx-auto" style="width: 100px" /> <br>
-            <p>I created using vuetify, postgres, hasura graphql on backend and 
-               vuejs on frontend to build this my official website </p>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn class="white--text" color="#0e152c" href="https://github.com/Gilchrist32/gcofficial-portfolio" target="_blank">
-            <v-icon left >folder_open</v-icon><span>View Github</span></v-btn>
-            <v-spacer></v-spacer>
-            <v-btn-icon>
-              <v-icon color="lime accent-4">mdi-thumb-up</v-icon>
-            </v-btn-icon>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-    <v-col>
-      <v-card mt-4 max-width="500" class="mx-auto mt-4" id="card-project">
-        <v-list-item>
-          <v-list-item-avatar size="70"><img src="../assets/photo/gilchrist.png" alt="gilchrist"></v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title class="headline white--text" id="head">GC Official</v-list-item-title>
-            <v-list-item-subtitle id="name">by Gilchrist Calunia</v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-        
-         <v-img src="../assets/project/portfolio.png" height="194" ></v-img>
-         
-         <v-card-text id="comment">
-           <h3>Clone Portfolio</h3>
+           <h3>{{ proj.title }}</h3>
            <hr class="lime accent-4 mb-2 mt-0 d-inline-block mx-auto" style="width: 150px" /> <br>
-           <p>I created using bootstrap and 
-             vuejs on frontend to build this my another syntax code or tag official website </p>
+           <p>{{ proj.description }}</p>
          </v-card-text>
          
          <v-card-actions>
-           <v-btn class="white--text" color="#0e152c" href="https://github.com/Gilchrist32/gilchristcalunia" target="_blank">
+           <v-btn class="white--text" color="#0e152c" :href="proj.source_code" target="_blank">
            <v-icon left>folder_open</v-icon><span>View Github</span></v-btn>
-           <v-spacer></v-spacer>
-           
-           <v-btn icon>
-             <v-icon color="lime accent-4">mdi-thumb-up</v-icon>
-           </v-btn>
-         </v-card-actions>
-  </v-card>
-    </v-col>
+            <v-spacer></v-spacer>
+            <v-btn
+            :loading="loading"
+            :disabled="loading"
+             color="#0e152c"
+            @click="like">
+              <v-icon color="lime accent-4">mdi-thumb-up</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
     </v-container>
   </div>
 </template>
+<script>
+import { PROJECT_QUERYS } from '../graphql/queries'
+import { PROJECT_SUBSCRIPTION } from '../graphql/subscriptions'
+
+export default {
+  data(){
+    return {
+      loading: false,
+      project: []
+    }
+  },
+
+  apollo: {
+    project: {
+      query:  PROJECT_QUERYS,
+      subscribeToMore: {
+        document: PROJECT_SUBSCRIPTION,
+        updateQuery(previousResult, { subscriptionData }) {
+          if (previousResult) {
+            return {
+              project: [
+                ...subscriptionData.data.project
+              ]
+            }
+          }
+        }
+      },
+      result ({ data }) {
+        this.project = data.project
+      }
+    }
+  }
+}
+</script>>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Righteous&family=Didact+Gothic&display=swap');
 
