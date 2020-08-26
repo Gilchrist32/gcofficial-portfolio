461d139e13d0fb01b7dc7a490249f320bcd71b16
Gilchrist32 <32stephencurry@gmail.com>
2020-08-26 19:10:41 +0800

    Updated Font size

--- src/components/Homeinfo.vue
+++ src/components/Homeinfo.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="bg">
-      <v-mask class="gradient d-flex justify-content-center align-items-center">
+  <div class="bg" id="wrap-banner">
+      <v-mask class="gradient d-flex justify-content-center align-items-center mb-12 mt-5">
           <v-container>
               <v-row>
-                  <div class="white-text text-center text-md-left col-md-6 mt-xl-5 mb-6 mt-3  ">
-                    <h1 id="mname" class="h6-responsive font-weight-bold">GILCHRIST CALUNIA</h1>
+                  <div class="white-text text-center col-md-12 mt-xl-5 mb-12">
+                    <h2 id="mname" class="text-uppercase">GILCHRIST CALUNIA</h2>
                     <h6 id="quo" class="mb-4 font-weight-bold">Information Technology, Front-end Web Developer.</h6>
                       <hr class="my-4"> 
                       <h3 class="ttitle" v-for=" i in cal_subscriber.slice(-1)" :key="i"> {{ i.id }} <span id="n">Subscribers</span></h3>
+                       <br>
+                   <v-btn depressed dark color="primary" class="mt-5" x-large @click.prevent="scroll" rounded>
+                      Show more <v-icon right>mdi-arrow-right</v-icon>
+                   </v-btn>
                   </div>
-                   <v-col class="mt-xl-1 ml-3">
+                   <!--<v-col class="mt-xl-1 ml-3">
                       <v-img src="../assets/photo/home.png" width="320px" height="300px"></v-img>
-                   </v-col>
+                   </v-col>-->
               </v-row>
           </v-container>
     </v-mask> 
@@ -21,9 +25,20 @@
 <script>
 import { GET_ALL_SUBSCRIBER_QUERY } from '../graphql/queries'
 import { GET_ALL_SUBSCRIBER_QUERY_SUBSCRIPTION } from '../graphql/subscriptions'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Homeinfo',
+
+  methods: {
+        scroll() {
+            document.querySelector('#about')
+                    .scrollIntoView({ behavior: 'smooth' })            
+        }
+    },
+    computed: {
+        ...mapState(['mode'])
+    },
 
   data () {
     return {
@@ -57,7 +72,7 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Righteous&family=Lobster&family=Balsamiq+Sans&display=swap');
 .bg{
-    background: url('../assets/photo/background.jpg');
+    background: url('../assets/photo/backstyle.jpeg');
 }
  #twel{
     font-family: 'Lobster';
@@ -65,11 +80,33 @@ export default {
     color: #ffffff;
     padding-top: 25px;
   }
-#mname{
-    font-family: 'Righteous';
-    color: #FFC400;
-    font-size: 45px;
+  .v-mask{
+    border-radius: 0 !important;
+    -moz-webkit-border-radius: 0;
+    min-height: 100vh;
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
+
+  #wrap-banner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
   }
+ 
+ h2 {
+            font-size: 4rem !important;
+            font-family: 'Teko', sans-serif !important;
+            text-shadow: 2px 3px #000000;
+            text-transform: uppercase;
+            margin: 0 !important; 
+            padding-top: 50px;
+            @media (max-width: 767px) {
+                font-size: 3rem !important;
+            }
+ }
+
 #quo{
     font-family: 'Lobster';
     font-size: 23px;
