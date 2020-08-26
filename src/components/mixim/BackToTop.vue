017476bc70ed14b97532ca159f8427d0a7cf0647
Gilchrist32 <32stephencurry@gmail.com>
2020-08-26 19:16:22 +0800

    Changed Color Floating Button

--- src/components/mixim/BackToTop.vue
+++ src/components/mixim/BackToTop.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <v-flex xs10>
-        <v-btn v-scroll='onScroll' v-show="fab" fab dark fixed bottom right color="#FFC400" @click="toTop">
-          <v-icon>arrow_circle_up</v-icon>
+        <v-btn v-scroll='onScroll' v-show="fab" fab dark fixed bottom right color="primary" @click="toTop">
+          <v-icon>keyboard_arrow_up</v-icon>
         </v-btn>
     </v-flex>
   </div>
